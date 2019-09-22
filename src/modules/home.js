c014611d49b624583d2bf28ac181b95323931a55
Martin Kruger <martin@martinkruger.com>
2019-09-22 22:38:54 +0200

    Basic feature and box style

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,35 +1,77 @@
-import Logo from './logo-text-home.svg';
+import Logo from './menu-logo-flatten.png';
 
 const home = (() => {
+  const createBox = (style, size) => {
+    const boxContainer = document.createElement('div');
+    if (size === 50) {
+      boxContainer.classList.add('box-style-1-container-50');
+    } else if (size === 100) {
+      boxContainer.classList.add('box-style-1-container-100');
+    }
+    /* if (style === 1) {
+    } */
+    return boxContainer;
+  };
+
+  const createSeparator = (type) => {
+    const separatorContainer = document.createElement('div');
+    if (type === 'box') {
+      separatorContainer.classList.add('box-separator-container');
+
+      const svgSeparator = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+      svgSeparator.setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:xlink', 'http://www.w3.org/1999/xlink');
+      // svgSeparator.setAttribute('viewbox', '0 0 90 90');
+      
+      svgSeparator.classList.add('svg-box-separator-container');
+
+      const svgSeparatorLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+      svgSeparatorLine.classList.add('svg-box-separator-line');
+      svgSeparatorLine.setAttribute('x1', '50%');
+      svgSeparatorLine.setAttribute('y1', '10%');
+      svgSeparatorLine.setAttribute('x2', '50%');
+      svgSeparatorLine.setAttribute('y2', '90%');
+      
+      svgSeparator.appendChild(svgSeparatorLine);
+      separatorContainer.appendChild(svgSeparator);
+      
+
+    }
+    return separatorContainer;
+  };
+
   const displayToPage = () => {
     console.log('home: displayToPage()');
 
     const mainDisplay = document.getElementById('main-content-display');
-    mainDisplay.textContent = 'HOME - Testing';
+    mainDisplay.textContent = '';
+
+    const homeTopContainer = document.createElement('div');
+    homeTopContainer.classList.add('home-top-container');
 
     const homeLogoContainer = document.createElement('div');
     homeLogoContainer.classList.add('home-logo-container');
+    const imgHomeLogo = new Image();
+    imgHomeLogo.src = Logo;
+    imgHomeLogo.classList.add('img-home-logo');
+    imgHomeLogo.classList.add('animate-slide-left-in');
 
-    const homeLogo = new Image();
-    homeLogo.src = Logo;
-    homeLogo.style.height = '100px';
-    homeLogo.style.width = '100px';
+    setTimeout(() => {
+      imgHomeLogo.classList.remove('animate-slide-left-in');
+    }, 50);
+    homeLogoContainer.appendChild(imgHomeLogo);
+    homeTopContainer.appendChild(homeLogoContainer);
+    mainDisplay.appendChild(homeTopContainer);
 
-    mainDisplay.appendChild(homeLogo);
-
-
-/*     const svgHomeLogo = document.createElement('img');
-    svgHomeLogo.setAttribute('src', './logo-text-home.svg');
-    svgHomeLogo.setAttribute('alt', 'Logo');
-    svgHomeLogo.classList.add('svg-home-logo');
-    homeLogoContainer.appendChild(svgHomeLogo); */
-
-
-
-    mainDisplay.appendChild(homeLogoContainer);
-
-
+    const homeFeatureContainer = document.createElement('div');
+    homeFeatureContainer.classList.add('home-feature-container');
+    homeFeatureContainer.appendChild(createBox(1, 100));
+    homeFeatureContainer.appendChild(createSeparator('box'));
+    homeFeatureContainer.appendChild(createBox(1, 100));
+    mainDisplay.appendChild(homeFeatureContainer);
   };
+
+
+
   return {
     displayToPage,
   };
